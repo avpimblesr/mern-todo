1e5c2b47c862e315d4ecdc3983709e4dc25c874d
avpimblesr <allan.pimble@gmail.com>
2020-11-23 12:51:05 -0500

    Fleshed out the AddTodo component

--- client/src/components/AddTodos.jsx
+++ client/src/components/AddTodos.jsx
@@ -1,23 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Consumer } from './Context';
 
 export default class AddTodos extends Component {
+  state = {
+    id: 4,
+    title: '',
+    completed: false
+  }
+
+  update = (e) => {
+    this.setState({
+      title: e.target.value
+    })
+  }
+
+  addTodo = (dispatch, e) => {
+    e.preventDefault()
+    const newTodo = this.state
+    if(!this.state.title) return
+    dispatch({ type: 'ADD', payload: newTodo })
+    this.setState({ title: '' })
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          <input 
-          type="text" 
-          className="form-control rounded-0" 
-          placeholder="Type your todo here ..."
-          />
+      <Consumer>{value => {
+        const { dispatch } = value
+        return (
+          <form onSubmit={this.addTodo.bind(this, dispatch)}>
+            <input
+              type="text"
+              className="form-control rounded-0"
+              placeholder="Type your todo here ..."
+              onChange={this.update} value={this.state.title}
+            />
 
-          <button
-            className="form-control rounded-0 btn-secondary"
-            type="submit">
-            Add Todo
+            <button
+              className="form-control rounded-0 btn-secondary"
+              type="submit"
+            >
+              Add Todo
             </button>
-        </form>
-      </div>
+          </form>
+        )
+      }}
+      </Consumer>
     )
   }
 }
